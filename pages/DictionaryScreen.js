dbb94eee18e74024a71547a1b632be1b5b0256e5
Thinhtucute <voduythinh2004@gmail.com>
2024-12-02 14:00:39 +0700

    add search bar

--- pages/DictionaryScreen.js
+++ pages/DictionaryScreen.js
@@ -1,21 +1,28 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheetm, ScrollView } from 'react-native';
+import CustomSearchBar from '../components/customSearchBar'; // Adjust the path as needed
+import styles from '../stylesheet';
 
 function DictionaryScreen() {
-    return (
-      <View style={styles.screen}>
-        <Text>Dictionary Screen</Text>
-      </View>
-    );
-}
+  const [searchQuery, setSearchQuery] = useState('');
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    }
-)
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    // Add logic to handle the search query, e.g., filter Dictionarys
+  };
+
+  return (
+    <View style={styles.container}>
+      <CustomSearchBar
+        placeholder="Search for words..."
+        iconUri="https://img.icons8.com/ios-filled/50/000000/search.png"
+        onChangeText={handleSearch}
+      />
+      <ScrollView contentContainerStyle={styles.contentScroll}>
+          {/* Add logic to display filtered Dictionarys based on searchQuery */}
+      </ScrollView>
+    </View>
+  );
+}
 
 export default DictionaryScreen;
