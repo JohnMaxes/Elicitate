85e7c83feb743664a346faafbaff5a82bb19f520
Thinhtucute <voduythinh2004@gmail.com>
2024-12-02 12:41:56 +0700

    Update UI

--- pages/LoginScreen.js
+++ pages/LoginScreen.js
@@ -1,82 +1,107 @@
-import {React, useState}from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, ScrollView, Image} from 'react-native';
-import axios from 'axios';
-import styles from "../stylesheet";
+import { React, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import CustomInput from '../components/customInput';
-const LoginForm = ({ togglePage, handleLogin }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const onLoginPress = () => {
-      const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    if (!email || !password) {
-      alert('Please fill out all fields.');
-      return;
-    }
-    else 
-    {
-      handleLogin();
-    /* enabling API enquiry to backend
-        axios.post(
-          'https://f8b7-116-109-144-43.ngrok-free.app/login',
-          {
-            usernameOrEmail: email,
-            password: password
-          },
-          config
-        )
-        .then(response => {
-          if(response.status == 200) handleLogin();
-          else alert('Wrong credentials!');
-        })
-        .catch(error => {
-          alert(error);
-        })
+import axios from 'axios';
+
+const LoginScreen = ({ togglePage, handleLogin }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onLoginPress = () => {
+    const config = {
+      headers: {
+          'Content-Type': 'application/json'
       }
-    */
-    };
-  }
-    return (
-      <ScrollView style={{backgroundColor:'#CCE6FA', margin:0, padding:0}}>
-        <View style={styles.header}>
-          <Image style={styles.headerImg} source={require('../assets/logoElicitate.png')}/>
-          <Text style={styles.heading}>Elicitate</Text>
-        </View>
-        <CustomInput placeholder="Email" placeholderTextColor="grey" iconUri="https://img.icons8.com/?id=63&format=png" value={email} onChangeText={setEmail}/>
-        <CustomInput placeholder="Password" placeholderTextColor="grey" secureTextEntry iconUri="https://img.icons8.com/?id=94&format=png" value={password} onChangeText={setPassword} />
-        <View style={styles.forgetContainer}>
-          <TouchableOpacity>
-            <Text style={{ color: "#d366a4" }}>Forgot Password?</Text>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={onLoginPress}>
-          <Text style={styles.buttonText}>LOG IN</Text>
-        </TouchableOpacity>
-        <View>
-          <Text style={styles.loginWith}>Or login with</Text>
-        </View>
-        <View style={styles.iconContainer}>
-          <TouchableOpacity>
-            <Image style={styles.socialImg} source={{ uri: "https://img.icons8.com/color/512/facebook-new.png" }} />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image style={[styles.socialImg, { width: 65, height: 68 }]} source={{ uri: "https://img.icons8.com/?size=512&id=17949&format=png" }} />
-          </TouchableOpacity>
-        </View>
-        <View style={[styles.toogleTextContainer, {marginTop:-10}]}>
-          <Text style={[styles.toggleText, { fontWeight: "normal", color: "black" }]}>
-            Don't have an account?{" "}
-          </Text>
-          <TouchableOpacity onPress={togglePage}>
-            <Text style={styles.toggleText}>Sign up here!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
   };
-export default LoginForm;
-  
+  if (!email || !password) {
+    alert('Please fill out all fields.');
+    return;
+  }
+  else 
+  {
+    handleLogin();
+  };
+}
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          style={styles.logo}
+          source={{ uri: 'https://i.ibb.co/HxXVqfS/image-removebg-preview.png' }}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>Login</Text>
+          <Text style={styles.subtitle}>Welcome back.</Text>
+        </View>
+        <CustomInput placeholder="Email" placeholderTextColor="grey" iconUri="https://img.icons8.com/?id=63&format=png" onChangeText={setEmail} />
+        <CustomInput placeholder="Password" placeholderTextColor="grey" secureTextEntry iconUri="https://img.icons8.com/?id=94&format=png" onChangeText={setPassword} />
+        
+        <TouchableOpacity style={styles.button} onPress={onLoginPress}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+        <View style={styles.signupContainer}>
+          <Text style={{fontSize: 16, fontStyle: 'italic'}}>Don't have an account? </Text>
+          <TouchableOpacity onPress={togglePage}>
+            <Text style={[styles.signupText, {fontSize: 16}]}>Sign up here!</Text>
+          </TouchableOpacity>
+        </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f1f4ff',
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    margin: 50,
+    alignContent: 'center',
+  },
+  textContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    marginLeft: 40,
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: '#2a4dbf',
+  },
+  subtitle: {
+    fontSize: 20,
+    marginBottom: 16,
+    fontStyle: 'italic',
+  },
+  button: {
+    backgroundColor: "#2a4dbf",
+    padding: 10,
+    borderRadius: 15,
+    width: "50%",
+    alignSelf: "flex-end",
+    marginTop: 20,
+    marginRight: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    paddingVertical: 5,
+    textAlign: "center",
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    marginTop: 100,
+    marginBottom: 20,
+  },
+  signupText: {
+    color: '#007BFF',
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginScreen;
