db2246abdfd7617be3bc7a4ce9af10cd3fa2bb03
Thinhtucute <voduythinh2004@gmail.com>
2024-12-02 14:01:00 +0700

    add search bar

--- pages/CourseScreen.js
+++ pages/CourseScreen.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+// CourseScreen.js
+import React, { useState } from 'react';
+import { View, Text, StyleSheetm, ScrollView } from 'react-native';
+import CustomSearchBar from '../components/customSearchBar'; // Adjust the path as needed
 import styles from '../stylesheet';
 
 function CourseScreen() {
-    return (
-      <View style={styles.screen}>
-        <Text>Course Screen</Text>
-      </View>
-    );
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    // Add logic to handle the search query, e.g., filter courses
+  };
+
+  return (
+    <View style={styles.container}>
+      <CustomSearchBar
+        placeholder="Find courses..."
+        iconUri="https://img.icons8.com/ios-filled/50/000000/search.png"
+        onChangeText={handleSearch}
+      />
+      <ScrollView contentContainerStyle={styles.contentScroll}>
+        <Text
+          style={[
+            styles.heading,
+            {
+              textAlign:'flex-start',
+              fontSize: 20,
+              marginLeft: -20,
+              marginTop: -20
+            }
+          ]}>My courses
+        </Text>
+          {/* Add logic to display filtered courses based on searchQuery */}
+      </ScrollView>
+    </View>
+  );
 }
- 
+
 export default CourseScreen;
