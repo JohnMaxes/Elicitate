c2144c842bd712c35010779cf1a8bb7767bf1488
JohnMaxes <baodang9855.tdn@gmail.com>
2024-12-09 15:51:44 +0700

    updated Backend (2)

---
+++ BACKEND/app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var userInfoRouter = require('./routes/user');
+var showRequestRouter = require('./routes/showReq');
+var registerRouter = require('./routes/register');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/user', userInfoRouter);
+app.use('/showReq', showRequestRouter);
+app.use('/register', registerRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Set the port
+const port = process.env.PORT || 3000;
+
+// Start the server and log the port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
---
+++ BACKEND/routes/login.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+
+const { app } = require('../config/firebase');
+const { getFirestore, collection, getDocs, addDoc, doc, setDoc, query, where, serverTimestamp } = require('firebase/firestore'); // Import Firestore functions
+const db = getFirestore(app);
+
+var encryption = require('../functions/passwordEncrypt');
+var createToken = require('../functions/token')
+
+router.post('/', async(req, res, next) => { 
+  const { usernameOrEmail, password } = req.body;
+  console.log(usernameOrEmail + ' ' + password);
+  try {
+      const usersRef = collection(db, 'users');
+      const q1 = query(
+          usersRef,
+          where('user', '==', usernameOrEmail),
+      );
+
+      const snapshot1 = await getDocs(q1);
+      if(!snapshot1.empty) // if username's found
+      {
+          const userDoc = snapshot1.docs[0];
+          const userData = userDoc.data();
+          const userPassword = userData.password;
+          if(encryption.compare(password, userPassword)) 
+          {
+              userEmail = userData.email;
+              token = await createToken(usernameOrEmail, userEmail);
+              res.status(200).json({ message: token });
+          }
+          else res.status(400).json({ message: 'Wrong password!'});
+      } 
+      else // if the user did not input username
+      {
+          const q2 = query( // generate query for searching email
+            usersRef,
+            where('email', '==', usernameOrEmail),
+          )
+
+          const snapshot2 = await getDocs(q2);
+          if(!snapshot2.empty) // if email's found
+          {
+            const userDoc = snapshot2.docs[0];
+            const userData = userDoc.data();
+            const userPassword = userData.password;
+            if(encryption.compare(password, userPassword)) {
+              userUser = userData.username;
+              token = createToken(userUser, usernameOrEmail);
+              res.status(200).json({ message: token });
+            }
+            else res.status(400).json({ message: 'Wrong password!'});
+          }
+          else res.status(400).json({ message: 'Wrong password!'});
+      }
+    } catch(error) {
+      console.error('Error registering user:', error);
+      res.status(500).json({ message: 'Error registering user.' });
+  }
+});
+
+module.exports = router;
