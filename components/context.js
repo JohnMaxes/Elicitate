c0a06ad1286df1dd9d7f741ab96ccabb30897dca
JohnMaxes <baodang9855.tdn@gmail.com>
2024-12-11 22:40:54 +0700

    Update context.js

--- components/context.js
+++ components/context.js
@@ -1,14 +1,15 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { getLearnedWordNumber } from './Database';
 
+// KHÔNG ĐƯỢC SET useEffect Ở ĐÂY
+// MUỐN KHỞI TẠO FIRST TIME THÌ XÀI SETTER SET NÓ TRONG useEffect CỦA SCREEN CẦN XÀI CONTEXT
 export const GlobalContext = createContext();
 export const Context = ({ children }) => {
     const [streakCount, setStreakCount] = useState(0);
-    const [wordCount, setWordCount] = useState(0)
+    const [wordCount, setWordCount] = useState(0);
 
-  return (
-      <GlobalContext.Provider value={{ streakCount, wordCount, setStreakCount, setWordCount }}>
-          {children}
-      </GlobalContext.Provider>
-  );
+    return (
+        <GlobalContext.Provider value={{ streakCount, wordCount, setStreakCount, setWordCount }}>
+            {children}
+        </GlobalContext.Provider>
+    );
 };
